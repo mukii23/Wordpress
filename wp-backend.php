5d6dfc56e7aa5161a44eec9d6a2d967008599e95
Mukii <mukii2307@gmail.com>
2020-03-30 21:15:09 +0530

    For wp admin

--- wp-backend.php
+++ wp-backend.php
@@ -1,6 +1,6 @@
- /** 
+ /*************
  **** Add the user's mobile number to their profile screen.
- */
+ **************/
  
 
 add_action('show_user_profile', 'profile_fields');
@@ -28,4 +28,40 @@ function save_user_profile_fields( $user_id ) {
    if ( !current_user_can( 'edit_user', $user_id ) ) { return false; }
 
    update_user_meta( $user_id, 'new_mobile', $_POST['mobile'] );
+}
+
+ /*************
+ **** Add menu at backend with custom link
+ **************/
+add_action( 'admin_menu', 'dashboard_url' );
+function dashboard_url() {
+    add_menu_page( 'dashboard_url', 'My Menu', 'read', 'my_slug', '', 'dashicons-feedback', 1 );
+}
+
+add_action( 'admin_menu' , 'dashboard_function' );
+function dashboard_function() {
+    global $menu;
+    $menu[1][2] = "http://www.mukii.com";
+}
+
+ /*************
+ **** Add css class at backend in 'body' element
+ **************/
+function mk_admin_body_class( $classes ) {
+    global $current_user;
+    foreach( $current_user->roles as $role )
+        $classes .= ' role-' . $role;
+    return trim( $classes );
+}
+add_filter( 'admin_body_class', 'mk_admin_body_class' );
+
+ /*************
+ **** Add custom menu
+ **************/
+add_filter( 'wp_nav_menu_items', 'mk_menu_link', 10, 1 );
+function mk_menu_link( $items ) {
+   if (is_user_logged_in()) {
+			   $items .= '<li class="nav-item menu-item menu-item-type-post_type menu-item-object-page"><a href="/dashboard/" class="nav-link">Dashboard</a></li>';
+   }
+   return $items;
 }
